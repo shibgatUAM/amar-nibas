1e33014ccb57985ccbf9b5d29f8058a40ebf096c
Shibgat Ullah Al Mahmud <shibgat@hotmail.com>
2025-07-08 23:14:17 +0600

    topnavbar done.

--- src/components/layout/TopNavbar.jsx
+++ src/components/layout/TopNavbar.jsx
@@ -1,11 +1,30 @@
+import { Mail, MapPin } from 'lucide-react';
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 import React from 'react';
 
 const TopNavbar = () => {
-    return (
-        <div>
-            <h1>Top Navbar</h1>
+  return (
+    <div>
+      <div className="bg-[#0B2C3D] text-white p-2 flex flex-col lg:flex-row gap-4 lg:justify-between items-center">
+        <div className="flex gap-4 items-center ml-8">
+          <div className="flex gap-2 items-center text-sm lg:text-lg">
+            <Mail color="#FF503C" size={18} />
+            info@amarnibas.com
+          </div>
+          <div className="flex gap-2 items-center text-sm lg:text-lg">
+            <MapPin color="#FF503C" size={18} />
+            Dhaka, Bangladesh
+          </div>
         </div>
-    );
+        <div className="flex gap-4 items-center mr-8">
+          <FaFacebookF />
+          <FaXTwitter />
+          <FaInstagram />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopNavbar;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,17 +1,33 @@
 import { createBrowserRouter } from 'react-router';
 import MainLayout from '../layouts/MainLayout';
 import Home from '@/pages/Home';
+import { Component } from 'react';
+import AllProperties from '@/pages/AllProperties';
+import DashboardLayout from '@/pages/dashboard/DashboardLayout';
+import Login from '@/pages/authentication/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     Component: MainLayout,
     children: [
-        {
-            index: true,
-            Component: Home
-        }
-    ]
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        index: 'all-properties',
+        Component: AllProperties,
+      },
+      {
+        index: 'dashboard',
+        Component: DashboardLayout,
+      },
+      {
+        index: 'login',
+        Component: Login,
+      },
+    ],
   },
 ]);
 
